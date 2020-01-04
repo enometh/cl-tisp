42c98781b22ff55f8c2228631e7c1ebf04166ac2
rig0rmortis <omar@pigeon.works>
2020-01-04 16:56:26 -0500

    add tisp.lisp

---
+++ src/tisp.lisp
@@ -0,0 +1,9 @@
+(in-package :cl-user)
+(defpackage cl-tisp
+  (:use :cl)
+  (:export :generate-template))
+(in-package :cl-tisp)
+
+;; read template and populate it from variables
+(defun generate-template (template variables)
+  (nil))
