9f16c590a4d4d57b0642247334539c159aeac2bb
Madhu <enometh@net.meer>
2021-01-13 10:45:45 +0530

    src/zenekindarl-backend.lisp: patch zenekindarl.backend:emit-parameters to accept symbol keyword arguments
    patch zenekindarl/src/backend.lisp: (emit-parameters): to accept
    symbols as keyword parameters. (This should be an around method but as
    of now the generic function has only one method specialized on T.)
    
    Note reloading zenekindarl requires patching the code again.
    
    this patches zenekindarl commit ea8cfa8 on 2017-11-20

--- src/zenekindarl-backend.lisp
+++ src/zenekindarl-backend.lisp
@@ -1,8 +1,22 @@
 (in-package "CL-TISP")
 
+;; PATCH zenekindarl/src/backend.lisp: (emit-parameters): to accept
+;; symbols as keyword parameters. (This should be an around method but
+;; as of now the generic function has only one method specialized on
+;; T.)  Note reloading zenekindarl requires patching the code again.
+;;
+(defvar *zenekindarl-backend-uses-keyword-p* t)
+
+(defmethod zenekindarl.backend::emit-parameters (backend)
+  (let ((syms (zenekindarl.backend::symbols backend)))
+    (unless *zenekindarl-backend-uses-keyword-p*
+      (setq syms (mapcar (lambda (sym) (list (list sym sym))) syms)))
+    (if syms `(&key ,@syms) ())))
+
 (defun zenekindarl-render (ztemplate-string cl-toml-table)
-  (let ((func (zenekindarl:compile-template-string
-	       :stream ztemplate-string)))
+  (let* ((*zenekindarl-backend-uses-keyword-p* nil)
+	 (func (zenekindarl:compile-template-string
+		:stream ztemplate-string)))
     (with-output-to-string (stream)
       (apply func stream
 	     (cl-tisp.util::make-plist-bindings-for-package
